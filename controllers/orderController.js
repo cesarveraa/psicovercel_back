032b118c4e4229597ba63408d6417088bb3fb0bf
Cesar Vera <soymateovera@gmail.com>
2025-05-25 01:26:19 -0400

    fix: update import statement for Order model to match filename case

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import fs from 'fs';
-import Order from '../models/order.js';
+import Order from '../models/Order.js';
 import { sendApprovalEmail } from '../services/emailOrdersService.js';
 
 // Crear un nuevo pedido
